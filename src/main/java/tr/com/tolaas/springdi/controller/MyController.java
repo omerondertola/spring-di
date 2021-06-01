db5f37ae92d70fe78279f88219d4d193ee762c37
omerondertola <omerondertola@gmail.com>
2021-06-01 21:40:39 +0300

    primary bean support added

--- src/main/java/tr/com/tolaas/springdi/controller/MyController.java
+++ src/main/java/tr/com/tolaas/springdi/controller/MyController.java
@@ -1,13 +1,21 @@
 package tr.com.tolaas.springdi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import tr.com.tolaas.springdi.services.GreetingService;
 
 @Controller
 public class MyController {
 
-    public String sayHello() {
-        System.out.println("Hello World!");
+    private final GreetingService greetingService;
 
+    @Autowired
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        System.out.println(greetingService.sayHello());
         return "Hi to everyone :)";
     }
 }
